92eefb49fa6aa0879be27d94a52114951c32a659
Dhamodharreddy1 <97889511+Dhamodharreddy1@users.noreply.github.com>
2022-01-17 17:48:26 +0530

    Add files via upload

---
+++ CSSselectors.js
@@ -0,0 +1,21 @@
+//CSS Selectors
+
+ //PUBLICATION DATE
+ vertical-align: inherit;
+ font-weight: bold;
+
+ //BIDDING DATE
+ vertical-align: inherit;
+ box-sizing: border-box;
+ font-weight: bold;
+ 
+ //OBJECT
+ vertical-align: inherit;
+ box-sizing: border-box;
+ font-weight: bold;
+
+ //All links related to the paricular tender
+ margin-top: -20px;
+ width: max-content;
+ box-sizing: border-box;
+ 
